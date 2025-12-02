be4cbcbc058b5585fb03a072a37ac763a9a92fc0
Willem Jan Glerum <wjglerum@gmail.com>
2025-12-02 09:20:15 +0100

    Use quarkus-wiremock for testing

--- src/test/java/nl/wjglerum/country/CountriesResourceTest.java
+++ src/test/java/nl/wjglerum/country/CountriesResourceTest.java
@@ -1,18 +1,51 @@
 package nl.wjglerum.country;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+import io.quarkiverse.wiremock.devservice.ConnectWireMock;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
+@ConnectWireMock
+@TestHTTPEndpoint(CountriesResource.class)
 public class CountriesResourceTest {
+
+    WireMock wiremock;
+
+    @BeforeEach
+    public void setupWireMock() {
+        var response = """
+                [
+                  {
+                    "name": "Netherlands",
+                    "alpha2Code": "NL",
+                    "capital": "Amsterdam",
+                    "currencies": [
+                      {
+                        "code": "EUR",
+                        "name": "Euro",
+                        "symbol": "€"
+                      }
+                    ]
+                  }
+                ]
+                """;
+        wiremock.register(get(urlEqualTo("/v2/name/netherlands")).willReturn(
+                aResponse().withStatus(200).withHeader("Content-Type", "application/json").withBody(response))
+        );
+    }
 
     @Test
     public void testCountryNameEndpoint() {
         given()
-                .when().get("/country/name/netherlands")
+                .when()
+                .get("/name/netherlands")
                 .then()
                 .statusCode(200)
                 .body("$.size()", is(1),
